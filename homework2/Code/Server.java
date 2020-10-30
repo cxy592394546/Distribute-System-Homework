ed774e47126a5c8a3880df2c7a21d69c25c0c358
xinyu <cxy592394546@gmail.com>
2020-10-30 11:35:41 +0800

    v2.1
    Add fault tolerance mechanism

--- homework2/Code/Server.java
+++ homework2/Code/Server.java
@@ -14,24 +14,37 @@ public class Server {
             //获取请求内容
             String info;
             StringBuilder result = new StringBuilder();
-            String line = null;
+            String line1 = null;
+            String line0 = null;
             while ((info=bufferedReader.readLine())!=null) {
-                System.out.println("Server端1---查询文件:"+"dblp_split_x.xml");
-		        System.out.println("Server端1---client请求为查询作者:"+info);
-                String file_path = "~/temp_file/dblp_split_x.xml";
-                String command = "your commond";
-                String[] params = new String[] {"/bin/sh", "-c", command};
-                Process process = Runtime.getRuntime().exec(params);
-                process.waitFor();
+                System.out.println("Server端1---查询文件:"+"dblp_split_1&6.xml");
+                System.out.println("Server端1---client请求为查询作者:"+info);
+                String file_path1 = "~/temp_file/dblp_split_1.xml";
+                String command1 = "cat " + file_path1 + " | grep -o \""+ info +"\" |wc -l\n";
+                String file_path0 = "~/temp_file/dblp_split_6.xml";
+                String command0 = "cat " + file_path0 + " | grep -o \""+ info +"\" |wc -l\n";
+                String[] params1 = new String[] {"/bin/sh", "-c", command1};
+                Process process1 = Runtime.getRuntime().exec(params1);
+                process1.waitFor();
 
-                BufferedReader bufrIn = new BufferedReader(new InputStreamReader(process.getInputStream(), "UTF-8"));
-                BufferedReader bufrError = new BufferedReader(new InputStreamReader(process.getErrorStream(), "UTF-8"));
+                BufferedReader bufrIn1 = new BufferedReader(new InputStreamReader(process1.getInputStream(), "UTF-8"));
+                BufferedReader bufrError1 = new BufferedReader(new InputStreamReader(process1.getErrorStream(), "UTF-8"));
 
-                while((line = bufrIn.readLine()) != null || (line = bufrError.readLine()) != null) {
-                    result.append(line);
+                while((line1 = bufrIn1.readLine()) != null || (line1 = bufrError1.readLine()) != null) {
+                    result.append(line1 + " ");
+                }
+                String[] params0 = new String[] {"/bin/sh", "-c", command0};
+                Process process0 = Runtime.getRuntime().exec(params0);
+                process0.waitFor();
+
+                BufferedReader bufrIn0 = new BufferedReader(new InputStreamReader(process0.getInputStream(), "UTF-8"));
+                BufferedReader bufrError0 = new BufferedReader(new InputStreamReader(process0.getErrorStream(), "UTF-8"));
+
+                while((line0 = bufrIn0.readLine()) != null || (line0 = bufrError0.readLine()) != null) {
+                    result.append(line0);
                 }
             }
-            System.out.println("Server端1:" + result + "次");
+            System.out.println("Server端1-dblp_split_1&6:" + result + "次");
             socket.shutdownInput();
 
             //向客户端发送数据
